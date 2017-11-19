3c1649c7d916512c000d127ceffca47905a4e185
Joe Nigma <joenigma33@gmail.com>
2017-11-19 20:01:33 +0200

    added comment

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,13 +3,14 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		int a =5;
+		int a = 5;
 		System.out.println(a);
 		double b = 0.9;
 		System.out.println(b);
 		System.err.println("Hi");
 		System.out.println("FFF");
 		System.out.println("dddd");
+		// di
 	}
 
 }
